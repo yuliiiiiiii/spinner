e27816369078366853b8151c2eb0ccbebf766137
Yuli Wang <yuliwang1007@gmail.com>
2023-08-01 20:32:53 -0400

    modify space

--- spinner1.js
+++ spinner1.js
@@ -1,11 +1,11 @@
 // process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r|   ');
 }, 100);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r/   ');
 }, 300);
 
 setTimeout(() => {
